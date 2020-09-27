bba3d50b809802486b39b0f078b7dbc6665c344f
Mohamed Omar Farook <omar.farook@aspiredigital.com>
2020-09-27 22:23:25 +0800

    Updates

---
+++ precache-manifest.10d75a62a42073804a669db274e69032.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "191cadc6bbcbd9849baca5f6b99517a1",
+    "url": "/autodesk_assignment/index.html"
+  },
+  {
+    "revision": "0ce823436fd6b0f7b5b8",
+    "url": "/autodesk_assignment/static/css/main.4618fe0a.chunk.css"
+  },
+  {
+    "revision": "82c5769eb908aeed3c8e",
+    "url": "/autodesk_assignment/static/js/2.8ab67194.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/autodesk_assignment/static/js/2.8ab67194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ce823436fd6b0f7b5b8",
+    "url": "/autodesk_assignment/static/js/main.7ca7d226.chunk.js"
+  },
+  {
+    "revision": "a3da442bfda7a9b9e0cc",
+    "url": "/autodesk_assignment/static/js/runtime-main.8f93c1e8.js"
+  }
+]);
